3f7ccc9c7a91229e8317526b5b21d18a68b528dc
akorchyn <artur.yurii.korchynskyi@ggxchain.io>
2023-12-29 17:46:37 +0200

    init repo

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05"; # We want to use packages from the binary cache
+    flake-utils.url = "github:numtide/flake-utils";
+    
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem 
+      (system:
+        let
+          pkgs  = import nixpkgs {
+            inherit system;
+          };
+
+        in rec {
+          devShells = {
+            default = pkgs.mkShell {
+                nativeBuildInputs = with pkgs; [
+                  nodejs-18_x
+                ];
+
+                buildInputs = [];
+            };
+          };
+        }
+      );
+} 
