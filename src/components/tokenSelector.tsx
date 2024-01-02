d39c4c710f15b330e59bfe84f95f9337d74f83d6
akorchyn <artur.yurii.korchynskyi@ggxchain.io>
2024-01-02 14:11:05 +0200

    more beautiful token selector

--- src/components/tokenSelector.tsx
+++ src/components/tokenSelector.tsx
@@ -1,10 +1,99 @@
+
+import { ChangeEvent, useState } from "react";
+import ReactSelect, { OnChangeValue } from "react-select";
+
+function mocked_tokens() {
+    return [
+        {
+            "name": "USDT",
+            "symbol": "usdt",
+            "network": "ERC20",
+        },
+        {
+            "name": "USDC",
+            "symbol": "usdc",
+            "network": "ERC20",
+        },
+        {
+            "name": "ATOM",
+            "symbol": "atom",
+            "network": "Cosmos",
+        },
+        {
+            "name": "ETH",
+            "symbol": "eth",
+            "network": "Ethereum",
+        }
+    ]
+}
+
+type Token = {
+    name: string;
+    symbol: string;
+    network: string;
+}
+
 export default function TokenSelector() {
+
+    const tokens = mocked_tokens();
+
+    const [selectedCrypto, setSelectedCrypto] = useState<Token | null>(tokens[0] as Token);
+    const [amount, setAmount] = useState<number>(0);
+
+    const handleSelectChange = (e: OnChangeValue<Token, false>) => {
+        setSelectedCrypto(e);
+        // Reset amount when crypto is changed
+        setAmount(0);
+    };
+
+    const handleAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setAmount(Number(e.target.value));
+    };
+
     return (
-        <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="usdt">USDT</option>
-            <option value="usdc">USDC</option>
-            <option value="atom">ATOM</option>
-            <option value="eth">ETH</option>
-        </select>
-    )
+        <div >
+            <div className="flex items-center justify-between [&>*]:text-white">
+                <ReactSelect
+                    value={selectedCrypto}
+                    onChange={handleSelectChange}
+                    options={tokens}
+                    className="h-full w-full"
+                    theme={(theme) => ({
+                        ...theme,
+                        borderRadius: 16
+                    })}
+                    styles={{
+                        control: (baseStyles, state) => ({
+                            ...baseStyles,
+                            backgroundColor: 'transparent',
+                            borderRadius: '1rem'
+                        }),
+                        option: (baseStyles, state) => ({
+                            ...baseStyles,
+                            borderRadius: '16px',
+                            marginBottom: '1px',
+                        }),
+                        menu: (baseStyles, state) => ({
+                            ...baseStyles,
+                            backgroundColor: 'transparent',
+                        }),
+                    }}
+
+                    formatOptionLabel={(token) => {
+                        return (
+                            <div className="flex items-center text-white w-full scale-95 hover:scale-100 transition-all transform-all duration-500 border-white">
+                                <img src={`/svg/${token.symbol}.svg`} className="w-10 h-10 my-1 mr-2" alt={`${token.name} icon`} />
+                                <p>{token.name}</p>
+                                <sup className="pl-1 ">{token.network}</sup>
+                            </div>
+                        );
+                    }}
+                />
+
+
+
+            </div>
+        </div>
+    );
+
 }
