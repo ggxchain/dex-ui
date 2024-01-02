33b3a8f74c14da08f666f34b2dd477385492facc
akorchyn <artur.yurii.korchynskyi@ggxchain.io>
2024-01-02 21:54:43 +0200

    clear, extracted state from the selector

--- src/components/dex.tsx
+++ src/components/dex.tsx
@@ -4,8 +4,21 @@ import { useState } from "react";
 import TokenSelector from "./tokenSelector";
 import Ruler from "./ruler";
 
+type FormData = {
+    sell_token: Token;
+    buy_token: Token;
+    sell_amount: number;
+    buy_amount: number;
+}
+
 export default function Dex() {
-    const [isMaker, setIsMaker] = useState(false);
+    const [isMaker, setIsMaker] = useState<boolean>(false);
+    const [formData, setFormData] = useState<FormData>({} as FormData);
+
+    const onClear = () => {
+        setFormData({} as FormData);
+    }
+
 
     return (
         <div className="text-slate-100 flex flex-col md:min-w-[50%] min-w-full p-10">
@@ -22,10 +35,10 @@ export default function Dex() {
 
             <div className="flex flex-col rounded-3xl secondary-gradient p-5 md:mx-[25px] mt-5">
                 <p className="text-sm">Sell</p>
-                <TokenSelector />
+                <TokenSelector token={formData.sell_token} amount={formData.sell_amount} onChange={(sell_token: Token, sell_amount: number) => { setFormData({ ...formData, sell_amount, sell_token }) }} />
 
                 <p className="text-sm mt-2">Buy</p>
-                <TokenSelector />
+                <TokenSelector token={formData.buy_token} amount={formData.buy_amount} onChange={(buy_token: Token, buy_amount: number) => { setFormData({ ...formData, buy_token, buy_amount }) }} />
 
                 <Ruler />
 
@@ -41,8 +54,8 @@ export default function Dex() {
                     </div>
 
                     <div className="flex justify-around mt-5">
-                        <button className="rounded-2xl border p-2 m-2 basis-1/4 grow-on-hover">Clear</button>
-                        <button className="basis-3/4 rounded-2xl border p-2 m-2 grow-on-hover">Connect wallet</button>
+                        <button className="rounded-2xl border p-2 m-2 basis-1/4 grow-on-hover" onClick={onClear}>Clear</button>
+                        <button className="basis-3/4 rounded-2xl border p-2 m-2 grow-on-hover" onClick={() => console.log(formData)}>Connect wallet</button>
                     </div>
                 </div>
             </div>
--- src/components/tokenSelector.tsx
+++ src/components/tokenSelector.tsx
@@ -1,25 +1,31 @@
 
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 import ReactSelect, { OnChangeValue } from "react-select";
 
-function mocked_tokens() {
+// Returns a list of tokens
+
+function mocked_tokens(): Token[] {
     return [
         {
+            "id": 1,
             "name": "USDT",
             "symbol": "usdt",
             "network": "ERC20",
         },
         {
+            "id": 2,
             "name": "USDC",
             "symbol": "usdc",
             "network": "ERC20",
         },
         {
+            "id": 3,
             "name": "ATOM",
             "symbol": "atom",
             "network": "Cosmos",
         },
         {
+            "id": 4,
             "name": "ETH",
             "symbol": "eth",
             "network": "Ethereum",
@@ -27,37 +33,43 @@ function mocked_tokens() {
     ]
 }
 
-type Token = {
-    name: string;
-    symbol: string;
-    network: string;
+interface TokenSelectorProps {
+    token: Token;
+    amount: number
+    onChange: (token_id: Token, amount: number) => void;
 }
 
-export default function TokenSelector() {
-
+export default function TokenSelector({ token, amount, onChange }: TokenSelectorProps) {
     const tokens = mocked_tokens();
 
-    const [selectedCrypto, setSelectedCrypto] = useState<Token | null>(tokens[0] as Token);
-    const [amount, setAmount] = useState<number | null>(null);
+    if (token === undefined) {
+        onChange(tokens[0], 0)
+        return <svg aria-hidden="true" className="md:w-10 md:h-10 w-6 h-6 self-center text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+        </svg>
+    }
 
     const handleSelectChange = (e: OnChangeValue<Token, false>) => {
-        setSelectedCrypto(e);
-        // Reset amount when crypto is changed
-        setAmount(0);
+        if (e === null) {
+            return;
+        }
+
+        onChange(e, 0)
     };
 
     const handleAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (Number(e.target.value) > 10000000) {
             return;
         }
-        setAmount(Number(e.target.value));
+        onChange(token, Number(e.target.value))
     };
 
     return (
         <div >
             <div className="flex items-center justify-between [&>*]:text-white">
                 <ReactSelect
-                    value={selectedCrypto}
+                    value={token}
                     onChange={handleSelectChange}
                     options={tokens}
                     className="h-full w-full"
@@ -95,7 +107,7 @@ export default function TokenSelector() {
                     }}
                 />
                 <div className="h-full [&>*]:bg-transparent border no-wrap border-l-0 w-24 md:w-32 text-right p-0.5 md:p-1.5 pr-2 rounded-r-[1rem] flex flex-col md:text-base text-xs">
-                    <input step="2" className="w-full text-right" type="number" placeholder="0.00" onChange={handleAmountChange} />
+                    <input value={amount} step="2" className="w-full text-right" type="number" placeholder="0.00" onChange={handleAmountChange} />
                     <p className="text-xs whitespace-nowrap">~= {Math.round((amount || 0.0) * 10)}$</p>
                 </div>
             </div>
