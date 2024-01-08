726a8ed7263bae1cb1a2c961ada077f50d37e0b5
Artur Yurii Korchynskyi <42449190+akorchyn@users.noreply.github.com>
2024-01-08 15:51:21 +0200

    Polkadot wallet (#7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function Layout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main className='min-h-screen h-max flex flex-col'>
+      <body className={`inter.className min-h-dvh`}>
+        <main className='h-max flex flex-col'>
           <HeaderWithNavbar />
           <div className="flex min-h-full h-max flex-col items-center justify-between md:ml-64">
             <div className="text-slate-100 flex md:h-[80vh] min-w-full p-5 md:mt-10 md:p-10 md:rounded-2xl md:bg-bg-gr-2/20">
