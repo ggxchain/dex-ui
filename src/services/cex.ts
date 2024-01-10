b7923a114c9eab7277621b3072eeafa3af077b81
Artur Yurii Korchynskyi <42449190+akorchyn@users.noreply.github.com>
2024-01-10 19:58:58 +0200

    FE: Centralized Exchange integration (#13)

--- src/services/cex.ts
+++ src/services/cex.ts
@@ -1,13 +1,59 @@
+import { EXCHANGE_PRICE_TTL } from "@/consts";
+
 type Prices = Map<string, number>;
 
+const cryptoCompareFetcher = async (tokens: string[]): Promise<Prices> => {
+    const tokenString = tokens.map((o) => o.toUpperCase()).join(",")
+
+    const json = await fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${tokenString}&tsyms=USD`)
+        .then(res => res.json())
+    const prices: Prices = new Map();
+    for (const i of tokens) {
+        const token = i.toUpperCase();
+        if (json[token] === undefined || json[token]["USD"] === undefined) {
+            console.error(`Failed to fetch price of ${token}`);
+            continue;
+        }
+        prices.set(token, json[token.toUpperCase()]["USD"]);
+    }
+    return prices;
+}
+
 export default class CexService {
-    async tokenPrice(token: string): Promise<number> {
-        return Promise.resolve(Math.random() * 10);
+    lastUpdated: number = 0;
+    cache: Prices = new Map<string, number>();
+    lastFetchedArray: string[] = [];
+
+    constructor() {
+        if (typeof window !== "undefined") {
+            this.lastUpdated = parseInt(window.localStorage.getItem("prices.lastUpdated") ?? "0");
+            this.cache = new Map(JSON.parse(window.localStorage.getItem("prices.cache") || "[]"));
+            this.lastFetchedArray = JSON.parse(window.localStorage.getItem("prices.lastFetchedArray") || "[]");
+        }
+    }
+
+    save() {
+        if (typeof window !== "undefined") {
+            window.localStorage.setItem("prices.lastUpdated", this.lastUpdated.toString());
+            window.localStorage.setItem("prices.cache", JSON.stringify(Array.from(this.cache.entries())));
+            window.localStorage.setItem("prices.lastFetchedArray", JSON.stringify(this.lastFetchedArray));
+        }
     }
 
     async tokenPrices(tokens: string[]): Promise<Prices> {
-        const prices = new Map<string, number>();
-        tokens.forEach(token => prices.set(token, Math.random() * 10));
-        return Promise.resolve(prices);
+        const now = new Date().getTime();
+        if (tokens.every((o) => this.cache.has(o)) && now - this.lastUpdated < EXCHANGE_PRICE_TTL) {
+            return this.cache;
+        } else {
+            // We need combine the tokens that we already fetched and the new tokens to avoid spamming.
+            const newTokens = tokens.filter((o) => !this.cache.has(o));
+            const allTokens = [...this.lastFetchedArray, ...newTokens];
+            const prices = await cryptoCompareFetcher(allTokens);
+            this.lastUpdated = now;
+            this.cache = prices;
+            this.lastFetchedArray = allTokens;
+            this.save();
+        }
+        return this.cache;
     }
 }
