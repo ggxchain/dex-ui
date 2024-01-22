283b7247e93b5b3ff6c081984668b5dc719d1889
akorchyn <artur.yurii.korchynskyi@ggxchain.io>
2024-01-22 13:47:53 +0200

    reused input, make smooth animation

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -6,13 +6,13 @@ type InputProps = React.ComponentPropsWithRef<'input'> & {
 };
 
 export function Input({ name, placeholder, wrapperClassName, className, ...props }: InputProps) {
-    const isValue = props.value !== undefined;
+    const isValue = props.value !== undefined && props.value !== null && props.value !== "";
     const inputPlaceholder = placeholder ?? name;
 
     return (
         <div className={`relative w-full h-full ${wrapperClassName ?? ''}`}>
             <input {...props} placeholder={inputPlaceholder} className={className} />
-            <p className={`absolute top-1/4 left-2 -translate-y-1/2 text-[75%] opacity-75 ${isValue ? "" : "hidden"}`}
+            <p className={`absolute top-1/4 left-2 -translate-y-1/2 text-[75%] ${isValue ? "opacity-75" : "opacity-0"} transition-opacity ease-in-out delay-150 duration-300`}
                 style={{ lineHeight: "1", marginBlockStart: "0" }}>
                 {name}
             </p>
@@ -27,19 +27,15 @@ type InputWithPriceInfoProps = InputProps & {
 
 export function InputWithPriceInfo(props: Readonly<InputWithPriceInfoProps>) {
     assert(typeof props.value === "number")
-    const isValue = props.value !== undefined;
     const value = props.value ?? 0;
+    // We don't want to pass wrapperClassName to Input component
+    const forwardProps = { ...props, wrapperClassName: '' };
 
-    // We can't reuse Input because managing margins with absolute become a nightmare
     return (<div className={`relative w-full h-full ${props.wrapperClassName ?? ''}`} >
-        <input {...props}
+        <Input {...forwardProps}
             type="number"
             value={value.toString()}
         />
         <p className="absolute bottom-0 opacity-50 right-2 top-1/2 -translate-y-1/2">{props.symbol}{value >= 2 ? "s" : ""} <span className="text-sm">(${props.price.toFixed(2)})</span></p>
-        <p className={`absolute top-1/4 left-2 -translate-y-1/2 text-[75%] opacity-75 ${isValue ? "" : "hidden"}`}
-            style={{ lineHeight: "1", marginBlockStart: "0" }}>
-            {props.name}
-        </p>
     </div>)
 }
