92e817a45b93ab7aebd517ad0b935b35d400bb88
AuroraLantean <raymondlieu.crypto@runbox.com>
2024-04-03 18:12:37 +0300

    add handleConnection, handleAccountSelection

--- src/app/bridge-btc/page.tsx
+++ src/app/bridge-btc/page.tsx
@@ -4,20 +4,29 @@ import { type ChangeEvent, useEffect, useState } from "react";
 import { web3Accounts, web3Enable, web3FromAddress } from '@polkadot/extension-dapp';
 import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 import { BN } from "@polkadot/util/bn";
-const wsProviderURL = "ws://127.0.0.1:9944";
+import dynamic from 'next/dynamic';
+import { GGX_WSS_URL } from "@/consts";
+//const wsProviderURL = "ws://127.0.0.1:9944";
 //https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944#/chainstate
 
 const lg = console.log;
-const NAME = 'GGX'
+const DAPP_NAME = 'GGX'
+// const MyClientComponent = dynamic(
+//   () => import('@/components/my-client-component/MyClientComponent').then(module => module.MyClientComponent) as any,
+//   { ssr: false },
+// ) as any;
 const BridgeBtc = () => {
   const [api, setApi] = useState<ApiPromise>();
+  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+  const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta>();
+
   const [balance, setBalance] = useState<BN>(new BN(0));
 
   const amount = new BN(10).mul(new BN(10).pow(new BN(12)))
 
   const setup = async () => {
     lg("setup()")
-    const wsProvider = new WsProvider(wsProviderURL);
+    const wsProvider = new WsProvider(GGX_WSS_URL);
     const api = await ApiPromise.create({ provider: wsProvider });
     setApi(api);
   }
@@ -27,20 +36,63 @@ const BridgeBtc = () => {
     setup();
   }, []);
 
-
   const clickOne = async () => {
     lg("clickOne()")
     if (!api) throw Error("No_API_found");
     const time = await api.query.timestamp.now();
     lg("timestamp:", time.toPrimitive());
     lg("amount:", amount.toString());
+  }
+  const handleConnection = async () => {
+    lg("handleConnection()")
+    if (typeof window !== "undefined") {
+      const extensions = await web3Enable(DAPP_NAME);//show how many extions you have installed
+      if (!extensions) { throw Error("No_extension _found") }
+      const allAccounts = await web3Accounts();
+      lg('allAccounts:', allAccounts);
 
+      setAccounts(allAccounts);
+      if (allAccounts.length === 1) {
+        setSelectedAccount(allAccounts[0]);
+      }
+    }
+  }
+  const handleAccountSelection = async (e: ChangeEvent<HTMLSelectElement>) => {
+    lg("handleAccountSelection")
+    //HTMLSelectElement is copied from onChange hint
+    if (typeof window !== "undefined") {
+      const selectedAddress = e.target.value;
+      //if(!address) { throw Error() }
+      const account = accounts.find(account => account.address === selectedAddress)
+      if (!account) { throw Error("No_account_found") }
+      lg('selectedAccount=', account)
+      setSelectedAccount(account)
+    }
   }
 
-  return (<div>Dapp
-    <span>.</span>
-    {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-    <button onClick={clickOne}>ClickOne</button>
+  return (<div>
+    <span>BTC to KBTC Bridge</span>
+    <br />
+    {accounts.length === 0 ? (
+      <button type="button" onClick={handleConnection}>Connect</button>) : null}
+
+    {accounts.length > 0 && !selectedAccount ? (<>
+      <select onChange={handleAccountSelection}>
+        <option value="Choose your account" disabled hidden>Choose your account</option>
+
+        {accounts.map((account) => (
+          <option key={account.address} value={account.address}>
+            {account.meta.name || account.address}
+          </option>
+        ))}
+      </select>
+    </>
+    ) : null}
+
+    {selectedAccount ? selectedAccount.address : null
+    }
+    <br />
+    <button type="button" onClick={clickOne}>ClickOne</button>
 
   </div>)
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,103 +6,107 @@ import { usePathname } from "next/navigation";
 import ThreeDotsImage from "./common/threeDots";
 
 export default function HeaderWithNavbar() {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	const menus = [
-		{
-			name: "Wallet",
-			path: "/wallet",
-		},
-		{
-			name: "Transfers",
-			path: "/transfer",
-		},
-		{
-			name: "DEX",
-			path: "/dex",
-		},
-	];
+  const menus = [
+    {
+      name: "Wallet",
+      path: "/wallet",
+    },
+    {
+      name: "Transfers",
+      path: "/transfer",
+    },
+    {
+      name: "DEX",
+      path: "/dex",
+    },
+    {
+      name: "Bridge-BTC",
+      path: "/bridge-btc",
+    },
+  ];
 
-	return (
-		<>
-			<header className="peer flex justify-between items-center secondary-gradient w-full sticky top-0 z-50 p-safe-or-4 ">
-				<button className="inline-flex items-center p-2 rounded-lg md:hidden">
-					<ThreeDotsImage />
-				</button>
+  return (
+    <>
+      <header className="peer flex justify-between items-center secondary-gradient w-full sticky top-0 z-50 p-safe-or-4 ">
+        <button className="inline-flex items-center p-2 rounded-lg md:hidden">
+          <ThreeDotsImage />
+        </button>
 
-				<div className="w-full flex justify-end">
-					<div className="w-[50%] text-GGx-gray text-base flex justify-end mr-[5%]">
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://ggxchain.io/wp-content/uploads/2023/10/GGx-Whitepaper.pdf"
-						>
-							Whitepaper
-						</Link>
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://ggxchain.io/#roadmap"
-						>
-							Roadmap
-						</Link>
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://ggxchain.io/developers/"
-						>
-							Developers
-						</Link>
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://ggxchain.io/news/"
-						>
-							News
-						</Link>
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://ggxchain.io/careers/"
-						>
-							Careeers
-						</Link>
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://docs.ggxchain.io"
-						>
-							Docs
-						</Link>
-						<Link
-							className="mx-auto grow-on-hover"
-							href="https://ggxchain.io/contacts"
-						>
-							Contact us
-						</Link>
-					</div>
-				</div>
-			</header>
+        <div className="w-full flex justify-end">
+          <div className="w-[50%] text-GGx-gray text-base flex justify-end mr-[5%]">
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://ggxchain.io/wp-content/uploads/2023/10/GGx-Whitepaper.pdf"
+            >
+              Whitepaper
+            </Link>
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://ggxchain.io/#roadmap"
+            >
+              Roadmap
+            </Link>
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://ggxchain.io/developers/"
+            >
+              Developers
+            </Link>
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://ggxchain.io/news/"
+            >
+              News
+            </Link>
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://ggxchain.io/careers/"
+            >
+              Careeers
+            </Link>
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://docs.ggxchain.io"
+            >
+              Docs
+            </Link>
+            <Link
+              className="mx-auto grow-on-hover"
+              href="https://ggxchain.io/contacts"
+            >
+              Contact us
+            </Link>
+          </div>
+        </div>
+      </header>
 
-			<aside className="text-GGx-light fixed top-0 left-0 z-40 w-80 h-screen transition-transform transparent p-safe-or-4 menu -translate-x-full peer-has-[button:focus]:translate-x-0 peer-has-[button:focus]:primary-gradient  md:translate-x-0">
-				<Image
-					src="/GGx.svg"
-					alt="GGX"
-					width={100}
-					height={100}
-					className="mx-auto"
-				/>
-				<div className="pt-20 px-3 py-10 overflow-y-auto h-full text-xl">
-					<ul className="space-y-10 font-medium flex flex-col items-center w-full">
-						{menus.map((menu, index) => (
-							<Link
-								key={index}
-								href={menu.path}
-								className={
-									"w-full text-center p-2 rounded-lg hover:bg-gray-800 " +
-									(pathname === menu.path ? "secondary-gradient" : "")
-								}
-							>
-								<li>{menu.name}</li>
-							</Link>
-						))}
-					</ul>
-				</div>
-			</aside>
-		</>
-	);
+      <aside className="text-GGx-light fixed top-0 left-0 z-40 w-80 h-screen transition-transform transparent p-safe-or-4 menu -translate-x-full peer-has-[button:focus]:translate-x-0 peer-has-[button:focus]:primary-gradient  md:translate-x-0">
+        <Image
+          src="/GGx.svg"
+          alt="GGX"
+          width={100}
+          height={100}
+          className="mx-auto"
+        />
+        <div className="pt-20 px-3 py-10 overflow-y-auto h-full text-xl">
+          <ul className="space-y-10 font-medium flex flex-col items-center w-full">
+            {menus.map((menu, index) => (
+              <Link
+                key={index}
+                href={menu.path}
+                className={
+                  "w-full text-center p-2 rounded-lg hover:bg-gray-800 " +
+                  (pathname === menu.path ? "secondary-gradient" : "")
+                }
+              >
+                <li>{menu.name}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </aside>
+    </>
+  );
 }
