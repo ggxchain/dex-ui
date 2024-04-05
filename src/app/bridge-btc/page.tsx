36241208b71601e954e1be7da1fbf9533353ebc5
AuroraLantean <raymondlieu.crypto@runbox.com>
2024-04-05 14:34:47 +0300

    add handleAmountChange, move wallet1 address to .env

--- src/app/bridge-btc/page.tsx
+++ src/app/bridge-btc/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { WsProvider, ApiPromise } from "@polkadot/api";
-import { type ChangeEvent, MouseEventHandler, useEffect, useState } from "react";
+import { type ChangeEvent, useEffect, useState } from "react";
 import { web3Accounts, web3AccountsSubscribe, web3Enable, web3FromAddress, web3FromSource } from '@polkadot/extension-dapp';
 import type { InjectedAccountWithMeta } from '@polkadot/extension-inject/types';
 import { BN } from "@polkadot/util/bn";
-import { createTestPairs } from "@polkadot/keyring/testingPairs";
 import { GGX_WSS_URL } from "@/consts";
 //const wsProviderURL = "ws://127.0.0.1:9944";
 //https://polkadot.js.org/apps/?rpc=ws%3A%2F%2F127.0.0.1%3A9944#/chainstate
@@ -16,11 +15,13 @@ const BridgeBtc = () => {
   const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
   const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta>();
 
-  const [balance, setBalance] = useState<BN>(new BN(0));
+  const [amountIp, setAmountIp] = useState(0);
+  const [balcWallet1GGXT, setBalcWallet1GGXT] = useState<BN>(new BN(0));
+  const [balcWallet1KBTC, setBalcWallet1KBTC] = useState<BN>(new BN(0));
   const addrAlice = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
-  const addr5ERyu = "5ERyuQCk9gt1SaTggiDReduDsgbhkYnUdAaLkCHZR7paEbuw";
+  const wallet1 = process.env.NEXT_PUBLIC_WALLET1 || "INVALID_WALLET_ADDRESS";
 
-  const amount = new BN(10).mul(new BN(10).pow(new BN(12)))
+  //const amount = new BN(10).mul(new BN(10).pow(new BN(12)));//.toString();
 
   let unsubscribe: () => void; // this is the function of type `() => void` that should be called to unsubscribe
 
@@ -47,33 +48,42 @@ const BridgeBtc = () => {
     setup();
   }, []);
 
-  const clickOne = async () => {
-    lg("clickOne()")
+  const checkBalances = async () => {
+    lg("checkBalances()")
     if (!api) throw Error("No_API_found");
     const time = await api.query.timestamp.now();
     lg("timestamp:", time.toPrimitive());
-    lg("amount:", amount.toString());
 
-    const address = selectedAccount?.address;
-    lg('selectedAccount.address:', address)
-    const entries = await api.query.tokens.accounts.entries(address)
-    // biome-ignore lint/complexity/noForEach: <explanation>
-    entries.forEach(entry => lg('token:', entry[0].toHuman()))
+    const selected = selectedAccount?.address;
+    lg('selectedAccount.address:', selected)
+    const entries = await api.query.tokens.accounts.entries(selected)
+    lg('show all detected tokens:')
+    for (const entry of entries) {
+      //lg('entries', entry)
+      lg(entry[0].toHuman())
+    }
 
     // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    const { free: ObjGGXT }: any = await api.query.tokens.accounts(address, { Token: 'GGXT' });
+    const { free: ObjGGXT }: any = await api.query.tokens.accounts(selected, { Token: 'GGXT' });
     lg("ObjGGXT:", ObjGGXT);
     lg("ObjGGXT:", ObjGGXT.toString(), ObjGGXT.toHuman());//1,180,591,620,717,411,303,424 or 2^70
 
     // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    const { free: ObjGGXT2 }: any = await api.query.tokens.accounts(addr5ERyu, { Token: 'GGXT' });
+    const { free: ObjGGXT2 }: any = await api.query.tokens.accounts(wallet1, { Token: 'GGXT' });
     lg("ObjGGXT2:", ObjGGXT2);
     lg("ObjGGXT2:", ObjGGXT2.toString(), ObjGGXT2.toHuman());
+    setBalcWallet1GGXT(new BN(ObjGGXT2.toString()))
 
     // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    const { free: ObjKBTC }: any = await api.query.tokens.accounts(address, { Token: 'KBTC' });
+    const { free: ObjKBTC }: any = await api.query.tokens.accounts(selected, { Token: 'KBTC' });
     //enum= token, foreignasset, lendtoken, lptoken, stablelptoken
     lg("ObjKBTC:", ObjKBTC.toString(), ObjKBTC.toHuman());
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    const { free: ObjKBTC2 }: any = await api.query.tokens.accounts(wallet1, { Token: 'KBTC' });
+    //enum= token, foreignasset, lendtoken, lptoken, stablelptoken
+    lg("ObjKBTC2:", ObjKBTC2.toString(), ObjKBTC2.toHuman());
+    setBalcWallet1KBTC(new BN(ObjKBTC2.toString()))
+
   }
   const handleConnection = async () => {
     lg("handleConnection()")
@@ -115,18 +125,31 @@ const BridgeBtc = () => {
     setSelectedAccount(account)
   }
 
+  //const amount = new BN(10).mul(new BN(10).pow(new BN(12)));//.toString();
+  const handleAmountChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const value = (event?.target as HTMLInputElement).value;
+    lg('handleAmountChange:', (event?.target as HTMLInputElement).value);
+    const out = Number.parseInt(value);
+    if (Number.isNaN(out)) {
+      console.error('parseInt failed');
+    } else {
+      setAmountIp(out);
+    }
+  }
   const handleTransaction = async () => {
     lg("handleTransaction")
     if (!api) throw Error("No_API_found");
     const beforeAccountData = await api.query.system.account(selectedAccount?.address);//addrAlice
     lg("beforeAccountData:", beforeAccountData.toHuman());
-    if (!selectedAccount) throw Error("No_selectedAccount");
+    if (!selectedAccount) throw Error("No_Selected_Account");
 
     if (typeof window !== "undefined") {
       const injector = await web3FromSource(selectedAccount?.meta.source);//(addrAlice);
 
-      //const txExtrinsic = api.tx.balances.transfer(BOB, 1000)
-      const hash = await api.tx.tokens.transferKeepAlive(addr5ERyu, { Token: 'GGXT' }, "1000").signAndSend(selectedAccount.address, { signer: injector.signer }, (result) => {
+      const amount = amountIp || 1000;
+      lg('amount:', amount);
+      //const txHash = api.tx.balances.transfer(BOB, 1000).signAndSend(alice);
+      const subscription = await api.tx.tokens.transferKeepAlive(wallet1, { Token: 'GGXT' }, `${amount}`).signAndSend(selectedAccount.address, { signer: injector.signer }, (result) => {
         //if (result.isCompleted)
         //if (result.isFinalized) 
         //if (result.isError)
@@ -139,23 +162,20 @@ const BridgeBtc = () => {
       }).catch((error: any) => {
         lg(':( transaction failed', error);
       });//addrAlice
-      lg("hash", hash);//subscription.unsubscribe()
+      lg("subscription", subscription);//subscription.unsubscribe()
     }
   }
-  /*async signerFor(address: PubKey): Promise<Signer> {
-      const { web3FromAddress } = await import("@polkadot/extension-dapp");
-      await this.inject();
-      return (await web3FromAddress(address)).signer;
-    }*/
+
   return (<div>
-    <span>BTC to KBTC Bridge</span>
-    <br />
+    <span>BTC to KBTC Bridge</span><br />
+    <span>Wallet1: {wallet1}</span><br />
+
     {accounts.length === 0 ? (
-      <button type="button" onClick={handleConnection}>Connect</button>) : null}
+      <button type="button" onClick={handleConnection}>Connect Wallet</button>) : null}
 
     {accounts.length > 0 && !selectedAccount ? (<>
-      <select onChange={handleAccountSelection}>
-        <option value="Choose your account" disabled hidden>Choose your account</option>
+      <select onChange={handleAccountSelection} defaultValue={'DEFAULT'}>
+        <option value="DEFAULT" disabled hidden>Choose your account</option>
 
         {accounts.map((account) => (
           <option key={account.address} value={account.address}>
@@ -169,9 +189,14 @@ const BridgeBtc = () => {
     {selectedAccount ? selectedAccount.address : null
     }
     <br />
-    <button type="button" onClick={clickOne}>ClickOne</button><br />
-    <button type="button" onClick={handleTransaction}>handleTransaction</button>
+    <button type="button" onClick={checkBalances}>Check Balances</button><br />
+    <input name="amount1" onChange={handleAmountChange} /><br />
+    <button type="button" onClick={handleTransaction}>Send Transaction</button><br />
+
+    Wallet1 GGXT Balance: {balcWallet1GGXT.toString()}
+    <br />
+    Wallet1 KBTC Balance: {balcWallet1KBTC.toString()}
 
   </div>)
-}
+}//disabled selected hidden
 export default BridgeBtc;
