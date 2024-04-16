4e318a11e46aca188861e870ab61b65c7e05d582
Bohdan <warchantua@gmail.com>
2024-04-16 19:57:50 +0300

    Add sst (#43)

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,21 @@ import { z } from "zod";
 // use zod to validate required env vars.
 // NEXT_PUBLIC_* env vars will be bundled in the frontend code.
 export const env = {
+	NODE_ENV: (() => {
+		return z.enum(["development", "production", "test"], {
+			required_error: "Must provide a NODE_ENV env var",
+		}).parse(process.env.NODE_ENV);
+
+	})(),
+
 	NEXT_PUBLIC_PARACHAIN_URL: (() => {
 		return z.string().url({message: "Must provide a URL to parachain node"}).parse(process.env.NEXT_PUBLIC_PARACHAIN_URL);
 	})(),
+
+	GGX_NETWORK: (() => {
+		return z.enum(["brooklyn", "sydney"], {
+			required_error: "Must provide a GGX_NETWORK env var",
+		}).parse(process.env.GGX_NETWORK);
+	})(),
+
 };
