87abfc314a4c3ff906e4fdfe39677baa682009d0
Bohdan Vanieiev <warchantua@gmail.com>
2024-04-16 18:31:49 +0200

    Add sst

---
+++ sst.config.ts
@@ -0,0 +1,27 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+import {env} from "./src/env";
+
+
+const getDomain = () => {
+  if (env.NODE_ENV === 'production') {
+    return `dex.${env.GGX_NETWORK}.ggxchain.io`;
+  }
+
+  throw new Error(`Unsupported NODE_ENV: ${env.NODE_ENV}`)
+}
+
+export default $config({
+  app(input) {
+    return {
+      name: "rfq-ui",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("DEX", {
+      domain: getDomain()
+    });
+  },
+});
