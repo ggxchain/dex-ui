4524d7ba8b7d87f95cb5e4a8c40481d0d5c15165
Bohdan Vanieiev <warchantua@gmail.com>
2024-04-23 11:00:47 +0200

    Add pre-commit

--- src/services/cex.ts
+++ src/services/cex.ts
@@ -19,3 +19,59 @@ const cryptoCompareFetcher = async (tokens: string[]): Promise<Prices> => {
 	}
 	return prices;
 };
+
+export default class CexService {
+	lastUpdated = 0;
+	cache: Prices = new Map<string, number>();
+	lastFetchedArray: string[] = [];
+
+	constructor() {
+		if (typeof window !== "undefined") {
+			this.lastUpdated = Number.parseInt(
+				window.localStorage.getItem("prices.lastUpdated") ?? "0",
+			);
+			this.cache = new Map(
+				JSON.parse(window.localStorage.getItem("prices.cache") || "[]"),
+			);
+			this.lastFetchedArray = JSON.parse(
+				window.localStorage.getItem("prices.lastFetchedArray") || "[]",
+			);
+		}
+	}
+
+	save() {
+		if (typeof window !== "undefined") {
+			window.localStorage.setItem(
+				"prices.lastUpdated",
+				this.lastUpdated.toString(),
+			);
+			window.localStorage.setItem(
+				"prices.cache",
+				JSON.stringify(Array.from(this.cache.entries())),
+			);
+			window.localStorage.setItem(
+				"prices.lastFetchedArray",
+				JSON.stringify(this.lastFetchedArray),
+			);
+		}
+	}
+
+	async tokenPrices(tokens: string[]): Promise<Prices> {
+		const now = new Date().getTime();
+		if (
+			tokens.every((o) => this.lastFetchedArray.includes(o)) &&
+			now - this.lastUpdated < EXCHANGE_PRICE_TTL
+		) {
+			return this.cache;
+		}
+		// We need combine the tokens that we already fetched and the new tokens to avoid spamming.
+		const newTokens = tokens.filter((o) => !this.lastFetchedArray.includes(o));
+		const allTokens = [...this.lastFetchedArray, ...newTokens];
+		const prices = await cryptoCompareFetcher(allTokens);
+		this.lastUpdated = now;
+		this.cache = prices;
+		this.lastFetchedArray = allTokens;
+		this.save();
+		return this.cache;
+	}
+}
