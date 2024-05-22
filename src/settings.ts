f4db3bb6480ae5a4904efd21974756984f0007c0
dev <dev@dev.dev>
2024-05-22 16:40:43 +0300

    Fix

--- src/settings.ts
+++ src/settings.ts
@@ -6,7 +6,8 @@ export const GGX_WSS_URL = env.NEXT_PUBLIC_PARACHAIN_URL;
 //TODO: We need to define it more precisely
 export const CONTRACT_MOCKED = false;
 export const CALCULATION_PRECISION = 3;
-export const NATIVE_TOKEN_ID_RESERVED = (env.NEXT_PUBLIC_GGX_NETWORK === 'sydney') ? 8888 : 8886;
+// biome-ignore format: it went crazy - formats locally and remotely differently, causing CI to fail
+export const NATIVE_TOKEN_ID_RESERVED = env.NEXT_PUBLIC_GGX_NETWORK === "sydney" ? 8888 : 8886;
 export const BLOCK_TIME_IN_MILLIS = 2000;
 export const MAX_DP = 8; //Integer. Max decimal precision
 export const PRICE_DP = 4; //INTEGER
