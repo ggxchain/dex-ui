99a385592c4d59c61f7d55c0a86037bd0a662627
Artur Yurii Korchynskyi <42449190+akorchyn@users.noreply.github.com>
2024-05-22 16:34:43 +0300

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,7 +6,7 @@ export const GGX_WSS_URL = env.NEXT_PUBLIC_PARACHAIN_URL;
 //TODO: We need to define it more precisely
 export const CONTRACT_MOCKED = false;
 export const CALCULATION_PRECISION = 3;
-export const NATIVE_TOKEN_ID_RESERVED = env.NATIVE_TOKEN_ID_RESERVED;
+export const NATIVE_TOKEN_ID_RESERVED = (env.NEXT_PUBLIC_GGX_NETWORK === 'sydney') ? 8888 : 8886;
 export const BLOCK_TIME_IN_MILLIS = 2000;
 export const MAX_DP = 8; //Integer. Max decimal precision
 export const PRICE_DP = 4; //INTEGER
